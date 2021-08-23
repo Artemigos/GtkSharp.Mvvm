233249571d7bed3f9e2e1b2a06135a145763ac41
Łukasz Klimek <klinn91@gmail.com>
2021-08-23 22:05:58 +0200

    implemented attach and detach for bindings

--- src/GtkSharp.Mvvm/Bindings/BindingsEngine.cs
+++ src/GtkSharp.Mvvm/Bindings/BindingsEngine.cs
@@ -9,19 +9,57 @@ namespace GtkSharp.Mvvm.Bindings
     /// </summary>
     public static class BindingsEngine
     {
-        public static IReadOnlyList<BindingDefinition> Definitions => definitions.AsReadOnly();
-        private static List<BindingDefinition> definitions = new List<BindingDefinition>();
+        public static IReadOnlyList<BindingDefinition> Definitions { get; }
+        private static readonly Dictionary<BindingDefinition, BindingRuntimeData> definitions;
 
         public static event EventHandler<BindingErrorEventArgs> BindingError;
 
+        static BindingsEngine()
+        {
+            definitions = new Dictionary<BindingDefinition, BindingRuntimeData>();
+            Definitions = definitions.Keys.ToList().AsReadOnly();
+        }
+
         public static void Attach(BindingDefinition binding)
         {
-            throw new NotImplementedException();
+            if (binding is null)
+            {
+                throw new ArgumentNullException(nameof(binding));
+            }
+
+            var data = definitions[binding] = new BindingRuntimeData();
+
+            if (binding.Mode == BindingMode.OneWay || binding.Mode == BindingMode.TwoWay)
+            {
+                binding.Source.Connect();
+                data.SourceHandler = obj => binding.Target.SetValue(binding.Convert(obj));
+                binding.Source.Subscribe(data.SourceHandler);
+            }
+
+            if (binding.Mode == BindingMode.OneWayToSource || binding.Mode == BindingMode.TwoWay)
+            {
+                binding.Target.Connect();
+                data.TargetHandler = obj => binding.Source.SetValue(binding.ConvertBack(obj));
+                binding.Target.Subscribe(data.TargetHandler);
+            }
         }
 
         public static void Detach(BindingDefinition binding)
         {
-            throw new NotImplementedException();
+            if (!definitions.TryGetValue(binding, out var data))
+                return;
+
+            if (binding.Mode == BindingMode.OneWay || binding.Mode == BindingMode.TwoWay)
+            {
+                binding.Source.Unsubscribe(data.SourceHandler);
+                binding.Source.Disconnect();
+            }
+
+            if (binding.Mode == BindingMode.OneWayToSource || binding.Mode == BindingMode.TwoWay)
+            {
+                binding.Target.Unsubscribe(data.TargetHandler);
+                binding.Target.Disconnect();
+            }
         }
 
         public static void DetachForObject(object target)
@@ -31,10 +69,16 @@ namespace GtkSharp.Mvvm.Bindings
                 throw new ArgumentNullException(nameof(target));
             }
 
-            definitions
+            Definitions
                 .Where(x => x.Source.IsDependentOn(target) || x.Target.IsDependentOn(target))
                 .ToList() // evaluate to avoid modifying source collection while enumerating
                 .ForEach(Detach);
+        }
+
+        private class BindingRuntimeData
+        {
+            public Action<object> SourceHandler;
+            public Action<object> TargetHandler;
         }
     }
 }
