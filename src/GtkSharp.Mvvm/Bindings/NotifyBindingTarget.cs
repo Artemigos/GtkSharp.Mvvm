4e25a236c64b7062c5adc662acc7e013c4589ea7
Łukasz Klimek <klinn91@gmail.com>
2021-08-23 20:57:28 +0200

    first implementation of the 'notify' binding target

--- src/GtkSharp.Mvvm/Bindings/NotifyBindingTarget.cs
+++ src/GtkSharp.Mvvm/Bindings/NotifyBindingTarget.cs
@@ -1,48 +1,92 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Reflection;
 
 namespace GtkSharp.Mvvm.Bindings
 {
     public class NotifyBindingTarget : IBindingTarget
     {
+        private readonly INotifyPropertyChanged target;
+        private readonly string propertyName;
+        private readonly List<Action<object>> subscriptions;
+        private readonly PropertyInfo property;
+        private bool connected = false;
+
         public NotifyBindingTarget(INotifyPropertyChanged target, string propertyName)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(propertyName))
+            {
+                throw new ArgumentException($"'{nameof(propertyName)}' cannot be null or empty.", nameof(propertyName));
+            }
+
+            this.target = target ?? throw new ArgumentNullException(nameof(target));
+            this.propertyName = propertyName;
+            this.subscriptions = new List<Action<object>>();
+
+            this.property = target.GetType().GetProperty(propertyName);
+            if (this.property == null)
+            {
+                throw new ArgumentException("Property does not exist on target.", nameof(propertyName));
+            }
         }
 
         public void Connect()
         {
-            throw new NotImplementedException();
+            if (this.connected)
+            {
+                throw new InvalidOperationException("Cannot connect twice.");
+            }
+
+            this.connected = true;
+            this.target.PropertyChanged += this.HandlePropertyChange;
         }
 
         public void Disconnect()
         {
-            throw new NotImplementedException();
+            if (!this.connected)
+            {
+                throw new InvalidOperationException("Cannot connect when disconnected.");
+            }
+
+            this.connected = false;
+            this.target.PropertyChanged -= this.HandlePropertyChange;
         }
 
         public object GetValue()
         {
-            throw new NotImplementedException();
+            return this.property.GetValue(this.target);
         }
 
         public bool IsDependentOn(object dependency)
         {
-            throw new NotImplementedException();
+            return ReferenceEquals(dependency, this.target);
         }
 
         public void SetValue(object value)
         {
-            throw new NotImplementedException();
+            this.property.SetValue(this.target, value);
         }
 
         public void Subscribe(Action<object> handler)
         {
-            throw new NotImplementedException();
+            this.subscriptions.Add(handler);
         }
 
         public void Unsubscribe(Action<object> handler)
         {
-            throw new NotImplementedException();
+            this.subscriptions.Remove(handler);
+        }
+
+        private void HandlePropertyChange(object sender, PropertyChangedEventArgs args)
+        {
+            if (args.PropertyName == this.propertyName)
+            {
+                foreach (var subscription in this.subscriptions)
+                {
+                    subscription(this.GetValue());
+                }
+            }
         }
     }
 }
